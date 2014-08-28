98ccf5098bd5c1d2e15675fad168bff19b8420bb
Robin Pokorny <Robin.Pokorny@lmc.eu>
2014-08-28 16:47:16 +0200

    JOBS-2820 Add profile options style

--- src/partials/navigation/index.js
+++ src/partials/navigation/index.js
@@ -3,6 +3,15 @@
 $(function () {
   $('[data-navigation-toggle]').click(function (e) {
     $('#navigation').toggleClass('navigation--open');
+    $(this).toggleClass('header__toggle--active');
+    e.preventDefault();
+  });
+});
+
+$(function () {
+  $('[data-navigation-profile-toggle]').click(function (e) {
+    $('#navigation-profile').toggleClass('navigation__profile--open');
+    $(this).toggleClass('header__toggle--active');
     e.preventDefault();
   });
 });
