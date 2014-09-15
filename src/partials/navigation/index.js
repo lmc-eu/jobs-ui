b82b862e97f86974a6fe1b8e63d45fc35103fd62
Jiri Novak <Jiri.Novak@lmc.eu>
2014-09-15 09:29:36 +0200

    Improve closing user menu

--- src/partials/navigation/index.js
+++ src/partials/navigation/index.js
@@ -1,17 +1,40 @@
 'use strict';
 
 $(function () {
-  $('[data-navigation-toggle]').click(function (e) {
-    $('#navigation').toggleClass('navigation--open');
-    $(this).toggleClass('header__toggle--active');
-    e.preventDefault();
-  });
-});
+  var navigation = $('#navigation');
+  var navigationToggle = $('[data-navigation-toggle]');
+  var navigationProfileToggle = $('[data-navigation-profile-toggle]');
+  var navigationProfile = $('#navigation-profile');
 
-$(function () {
-  $('[data-navigation-profile-toggle]').click(function (e) {
-    $('#navigation-profile').toggleClass('navigation__profile--open');
-    $(this).toggleClass('header__toggle--active');
+  navigationToggle.on('click', function (e) {
+    navigation.toggleClass('navigation--open');
+    navigationToggle.toggleClass('header__toggle--active');
     e.preventDefault();
+    e.stopImmediatePropagation();
+  });
+
+  navigationProfileToggle.on('click', function (e) {
+    navigationProfile.toggleClass('navigation__profile--open');
+    navigationProfileToggle.toggleClass('header__toggle--active');
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  });
+
+  navigation.on('click', function (e) {
+    e.stopImmediatePropagation();
+  });
+  navigationProfile.on('click', function (e) {
+    e.stopImmediatePropagation();
+  });
+
+  $('body').on('click', function () {
+    if (navigationProfileToggle[0]) {
+      navigationProfile.removeClass('navigation__profile--open');
+      navigationProfileToggle.removeClass('header__toggle--active');
+    }
+    if (navigationToggle[0]) {
+      navigation.removeClass('navigation--open');
+      navigationToggle.removeClass('header__toggle--active');
+    }
   });
 });
