8759c2e1d11ccae8345e9e8f8197d7847747651a
Jiri Novak <Jiri.Novak@lmc.eu>
2014-09-24 09:15:08 +0200

    Fix indentation in js files

--- src/partials/navigation/index.js
+++ src/partials/navigation/index.js
@@ -1,40 +1,40 @@
 'use strict';
 
 $(function () {
-  var navigation = $('#navigation');
-  var navigationToggle = $('[data-navigation-toggle]');
-  var navigationProfileToggle = $('[data-navigation-profile-toggle]');
-  var navigationProfile = $('#navigation-profile');
+    var navigation = $('#navigation');
+    var navigationToggle = $('[data-navigation-toggle]');
+    var navigationProfileToggle = $('[data-navigation-profile-toggle]');
+    var navigationProfile = $('#navigation-profile');
 
-  navigationToggle.on('click', function (e) {
-    navigation.toggleClass('navigation--open');
-    navigationToggle.toggleClass('header__toggle--active');
-    e.preventDefault();
-    e.stopImmediatePropagation();
-  });
+    navigationToggle.on('click', function (e) {
+        navigation.toggleClass('navigation--open');
+        navigationToggle.toggleClass('header__toggle--active');
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    });
 
-  navigationProfileToggle.on('click', function (e) {
-    navigationProfile.toggleClass('navigation__profile--open');
-    navigationProfileToggle.toggleClass('header__toggle--active');
-    e.preventDefault();
-    e.stopImmediatePropagation();
-  });
+    navigationProfileToggle.on('click', function (e) {
+        navigationProfile.toggleClass('navigation__profile--open');
+        navigationProfileToggle.toggleClass('header__toggle--active');
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    });
 
-  navigation.on('click', function (e) {
-    e.stopImmediatePropagation();
-  });
-  navigationProfile.on('click', function (e) {
-    e.stopImmediatePropagation();
-  });
+    navigation.on('click', function (e) {
+        e.stopImmediatePropagation();
+    });
+    navigationProfile.on('click', function (e) {
+        e.stopImmediatePropagation();
+    });
 
-  $('body').on('click', function () {
-    if (navigationProfileToggle[0]) {
-      navigationProfile.removeClass('navigation__profile--open');
-      navigationProfileToggle.removeClass('header__toggle--active');
-    }
-    if (navigationToggle[0]) {
-      navigation.removeClass('navigation--open');
-      navigationToggle.removeClass('header__toggle--active');
-    }
-  });
+    $('body').on('click', function () {
+        if (navigationProfileToggle[0]) {
+            navigationProfile.removeClass('navigation__profile--open');
+            navigationProfileToggle.removeClass('header__toggle--active');
+        }
+        if (navigationToggle[0]) {
+            navigation.removeClass('navigation--open');
+            navigationToggle.removeClass('header__toggle--active');
+        }
+    });
 });
--- src/svg-src-fallback.js
+++ src/svg-src-fallback.js
@@ -5,13 +5,13 @@
  */
 
 $(function () {
-  function supportsSvg () {
-    return document.implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#Image', '1.1');
-  }
+    function supportsSvg() {
+        return document.implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#Image', '1.1');
+    }
 
-  if (!supportsSvg()) {
-    $('img[src$=svg]').attr('src', function (i, src) {
-      return src.replace(/svg$/, 'png');
-    });
-  }
+    if (!supportsSvg()) {
+        $('img[src$=svg]').attr('src', function (i, src) {
+            return src.replace(/svg$/, 'png');
+        });
+    }
 });
