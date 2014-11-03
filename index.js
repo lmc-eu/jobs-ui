2933e26651d85d14d9321fa81e5a2aa9feb3a631
Martin Kristof <martin.kristof@lmc.eu>
2014-11-03 11:37:37 +0100

    JOBS-2824 namespace added, documentation content edited

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ require('./src/modules/layout');
 require('./src/partials/navigation');
 require('./src/components/listing');
 require('./src/components/inputs');
-require('./src/components/tabs');
 require('./src/svg-src-fallback');
 
 require('emerald');
 require('src-n-polyfill');
 require('remodal');
+
+window.JobsUI = {
+    Tabs: require('./src/components/tabs')
+};
--- src/components/tabs/index.js
+++ src/components/tabs/index.js
@@ -1,40 +1,38 @@
 'use strict';
 
-window.Tabs = (function () {
+/**
+ * @param {String} tabsCollection, name of collection (specifies a collection of elements which would be toggled)
+ * @param {String} activeClass, name of active class, default 'tabs__link--active'
+ * @constructor
+ */
+function Tabs(tabsCollection, activeClass) {
+    var self = this;
 
-    /**
-     * @param {String} tabsCollection, name of collection (specifies a collection of elements which would be toggled)
-     * @param {String} activeClass, name of active class, default 'tabs__link--active'
-     * @constructor
-     */
-    function Tabs(tabsCollection, activeClass) {
-        var self = this;
+    self.tabs = $('[data-tab-toggle][href^=#' + tabsCollection + ':]');
+    self.panels = $('[id^="' + tabsCollection + ':"]');
+    self.activeClass = activeClass || 'tabs__link--active';
 
-        self.tabs = $('[data-tab-toggle][href^=#' + tabsCollection + ':]');
-        self.panels = $('[id^="' + tabsCollection + ':"]');
-        self.activeClass = activeClass || 'tabs__link--active';
+    $('[data-tab-toggle]').on('click', function (e) {
+        e.preventDefault();
+        self.process(this);
+    });
+}
 
-        $('[data-tab-toggle]').on('click', function (e) {
-            e.preventDefault();
-            self.process(this);
-        });
-    }
+Tabs.prototype.process = function (el) {
+    var $el = $(el);
+    var target = $el.attr('href').substring(1);
 
-    Tabs.prototype.process = function (el) {
-        var $el = $(el);
-        var target = $el.attr('href').substring(1);
+    this.tabs.removeClass(this.activeClass);
+    $el.addClass(this.activeClass);
 
-        this.tabs.removeClass(this.activeClass);
-        $el.addClass(this.activeClass);
+    this.panels
+        .hide()
+        .filter('[id="' + target + '"]')
+        .show();
+};
 
-        this.panels
-            .hide()
-            .filter('[id="' + target + '"]')
-            .show();
-    };
+module.exports = Tabs;
 
-    return Tabs;
-})();
 
 
 
