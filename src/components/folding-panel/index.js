4cbcd715f01b5cb9bc3b7c9d6abaf5da52abee3e
Jan Dlouhy <jan.dlouhy@lmc.eu>
2015-03-12 09:44:55 +0100

    EDU-1798 Changed class names

--- src/components/folding-panel/index.js
+++ src/components/folding-panel/index.js
@@ -5,7 +5,7 @@ $(function () {
     var iconDownClass = 'icon--arrow-down';
 
     var setClasses = function (element) {
-        var span = element.find('.folding-panel__title .icon, .folding-panel__arrow .icon');
+        var span = element.find('.folding-panel__head .icon, .folding-panel__arrow .icon');
         if (span.hasClass(iconDownClass)) {
             span.removeClass(iconDownClass).addClass(iconRightClass);
         } else {
@@ -13,9 +13,9 @@ $(function () {
         }
     };
 
-    $('.folding-panel-list').on('click', '.folding-panel__arrow, .folding-panel__title', function () {
+    $('.folding-panel-list').on('click', '.folding-panel__arrow, .folding-panel__head', function () {
         var element = $(this).closest('.folding-panel');
         setClasses(element);
-        element.find('.folding-panel__additional-info').slideToggle('fast');
+        element.find('.folding-panel__body').slideToggle('fast');
     });
 });
